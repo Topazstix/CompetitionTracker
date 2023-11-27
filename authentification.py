7eb5722f9db8b299ce087f6202dd2c3787407002
Topazstix <r.drone@protonmail.ch>
2023-11-26 17:51:08 -0700

    Fix authentification environment vars loading

--- authentification.py
+++ authentification.py
@@ -3,9 +3,9 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
-username = os.environ.get("DJANGO_SUPERUSER_USERNAME", "admin")
-email = os.environ.get("DJANGO_SUPERUSER_EMAIL", "admin@example.com")
-password = os.environ.get("DJANGO_SUPERUSER_PASSWORD", "adminpassword")
+username = os.environ("DJANGO_SUPERUSER_USERNAME")
+email = os.environ("DJANGO_SUPERUSER_EMAIL")
+password = os.environ("DJANGO_SUPERUSER_PASSWORD")
 
 if not User.objects.filter(username=username).exists():
     User.objects.create_superuser(username, email, password)
